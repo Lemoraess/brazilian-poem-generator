f5613a5937b05fa243ad9d79a021938a96fe6715
Lenise Moraes <lemoraes.santana@gmail.com>
2025-04-18 10:20:18 -0300

    Inject and instal AI API, with integration on the html

--- src/script.js
+++ src/script.js
@@ -1,12 +1,25 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
+  console.log("gerando poema");
   new Typewriter("#poem", {
-    strings: "a paz invadiu o meu coração",
+    strings: response.data.answer,
     autoStart: true,
     cursor: "",
     delay: 50,
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let suggestionWord = document.querySelector("#inputBox");
+  let apiKey = "5btac2eb2ed52b236021789ofe9a3348";
+  let prompt = `generate a short brazilian poem with the ${suggestionWord.value}, in four short lines, maximum 30 characters per line, show just the poem, and sign with "Seu AI" in <strong> element`;
+  let context = "You are an experienced brazilian poet";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
+  //let poem = document.querySelector("#Poem");
+  //poem.innerHTML = "Generating a Poem for you, aguarde...";
 }
 
 let poemFormGenerator = document.querySelector("#generatorPoem");
