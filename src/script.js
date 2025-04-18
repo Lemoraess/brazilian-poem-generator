09a13b479284cacfb86ed08ddc47df33abf17e45
Lenise Moraes <lemoraes.santana@gmail.com>
2025-04-18 11:14:44 -0300

    Better UX performance, adjust AI and design

--- src/script.js
+++ src/script.js
@@ -13,13 +13,15 @@ function generatePoem(event) {
 
   let suggestionWord = document.querySelector("#inputBox");
   let apiKey = "5btac2eb2ed52b236021789ofe9a3348";
-  let prompt = `generate a short brazilian poem with the ${suggestionWord.value}, in four short lines, maximum 30 characters per line, show just the poem, and sign with "Seu AI" in <strong> element`;
+  let prompt = `In four short lines, on basic HTML, generate a short brazilian poem with the ${suggestionWord.value}, show JUST the poem, for example "Rosas são vermelhas </br> lirios azuis </br> eu te amo </br> e isso não rima</br> Seu AI" and sign with "Seu AI" in <strong> element`;
   let context = "You are an experienced brazilian poet";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="animate__animated animate__bounce animate__delay-0.5s">Generatind a Brazilian poem about ${suggestionWord.value}</div>`;
+
   axios.get(apiUrl).then(displayPoem);
-  //let poem = document.querySelector("#Poem");
-  //poem.innerHTML = "Generating a Poem for you, aguarde...";
 }
 
 let poemFormGenerator = document.querySelector("#generatorPoem");
